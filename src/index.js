f3ef08b4bfaf065b796c045af41b9220935e440f
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-09-09 11:04:22 +0200

    imported header component and routes to index of the project

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 
-// Header
+import Header from "components/header";
+import Route from "components/route";
+import routes from "router";
 // Route
 // routes
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      {/*Header*/}
+      <Header />
       <Switch>
-        {routes.map("Route")}
-        <Redirect to="/"/>
+        {routes.map(Route)}
+        <Redirect to="/" />
       </Switch>
       {/*Footer*/}
     </Router>
