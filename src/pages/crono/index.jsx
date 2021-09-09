35be43e32ca43deb93eabd8d5ab81cd560f6ae38
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-09-09 21:19:53 +0200

    bug in split function solved

--- src/pages/crono/index.jsx
+++ src/pages/crono/index.jsx
@@ -56,7 +56,8 @@ export default function Home({ enableCrono }) {
   };
 
   const handleClickSplit = () => {
-    setPrintTime((prevtime) => [...prevtime, cronoTime]);
+    setPrintTime((prevtime) => [...prevtime, { ...cronoTime }]);
+    // Recuerda! [...prevTime] nos asegura que lo que habia antes en el array no se borra con la nueva actualizacion. {...cronoTime} guarda una copia del objeto. Si ponemos solo el cronoTime este es el mismo objeto que se va acutalizando constantemente.
   };
 
   return (
